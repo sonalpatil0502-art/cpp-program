283c55edcc0416cff2a3114c6069ff49e588d352
sonalpatil0502-art <sonalpatil0502@gmail.com>
2026-07-29 14:57:30 +0000

    change

---
+++ swap2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    cout<<"After swapping:"<<a<<" "<<b;
+    return 0;
+
+}
